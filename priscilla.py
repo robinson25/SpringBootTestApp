74cf69a48e81dbab6c6ff01649aceb2b2f51acfb
robinson25 <robin.prisci@gmail.com>
2021-04-23 15:49:59 +0530

    FIRST COMMIT

---
+++ priscilla.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from kaggle_datasets import KaggleDatasets
+import matplotlib.pyplot as plt
+import PIL
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+
+print(tf.__version__)
